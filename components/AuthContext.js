bfc301dc26cf98a9942ec680591637bacfada1b5
efficient-life <efficient122@gmail.com>
2022-06-22 12:08:49 +0900

    create user

--- components/AuthContext.js
+++ components/AuthContext.js
@@ -1,43 +1,57 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import firebase, { auth } from '../firebase/firebase'
+import firebase, { auth, db } from '../firebase/firebase'
 
 const AuthContext = createContext()
 
 export const useAuth = () => {
-    return useContext(AuthContext)
+	return useContext(AuthContext)
 }
 
 const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+	const [currentUser, setCurrentUser] = useState(null)
+	const [loading, setLoading] = useState(true)
 
-    const login = () => {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        return auth.signInWithRedirect(provider)
-    }
+	const login = () => {
+		const provider = new firebase.auth.GoogleAuthProvider()
+		auth
+			.signInWithPopup(provider)
+			.then(result => {
+				const user = result.user
+				const userRef = db.collection('users').doc(user.uid)
+				userRef.set({
+					name: user.displayName,
+					photoURL: user.photoURL,
+					email: user.email,
+					likePostCount: 0,
+					createTime: firebase.firestore.FieldValue.serverTimestamp(),
+					updateTime: firebase.firestore.FieldValue.serverTimestamp(),
+				})
+			})
+			.catch(error => {
+				console.log(error)
+			})
 
-    const logout = () => {
-        return auth.signOut()
-    }
+		return
+	}
 
-    useEffect(() => {
-        return auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-    }, [])
+	const logout = () => {
+		return auth.signOut()
+	}
 
-    const value = {
-        currentUser,
-        login,
-        logout
-    }
+	useEffect(() => {
+		return auth.onAuthStateChanged(user => {
+			setCurrentUser(user)
+			setLoading(false)
+		})
+	}, [])
 
-    return (
-        <AuthContext.Provider value={value}>
-            {loading ? <p>loading...</p> : children}
-        </AuthContext.Provider>
-    )
+	const value = {
+		currentUser,
+		login,
+		logout,
+	}
+
+	return <AuthContext.Provider value={value}>{loading ? <p>loading...</p> : children}</AuthContext.Provider>
 }
 
 export default AuthProvider
