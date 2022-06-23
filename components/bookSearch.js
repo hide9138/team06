3155c7e663e3904e2820284b8918b8f52a113678
Yuki Sato <s1932058jl@s.chibakoudai.jp>
2022-06-23 19:24:18 +0900

    [Update] コンポーネントの切り出し

---
+++ components/bookSearch.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import BookCard from './bookCard'
+import styles from '../styles/components/bookSearch.module.css'
+
+
+// type Book = {
+//   id: string
+//   title: string
+//   authors: string
+//   description: string
+//   pageCount: number
+//   smallImageLink: string
+//   ImageLink: string
+// }
+
+const _results = [
+  {
+    id: 'fwaojf3wpjwnlw',
+    title: '本のタイトル',
+    authors: 'うんこまん',
+    description: '',
+    pageCount: 100,
+    smallImageLink: "",
+    ImageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
+  },
+  {
+    id: 'woawjofapwjw30ur',
+    title: '本のタイトル',
+    authors: 'うんこまん',
+    description: '',
+    pageCount: 100,
+    smallImageLink: "",
+    ImageLink: 'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png',
+  },
+]
+
+const BookSearchbar = () => {
+  // Book API から取得した結果用
+  const [results, setResults] = useState(_results)
+  // 入力値のデータ
+  const [searchWord, setSearchWord] = useState("")
+
+  const handleSearch = async () => {
+    if (!searchWord) return;
+
+    const searchedResults = await GetBooks(searchWord);
+    setResults(searchedResults)
+  }
+
+
+  return (
+    <div className={styles.book__search}>
+      <div className={styles.search__container}>
+        <div className={styles.search__icon}>
+          <Image src="/search.svg" width="16" height="16" alt="search icon" />
+        </div>
+        <input className={styles.search__input} type="text" placeholder="本のタイトル、著者名" onClick={(e) => setSearchWord(e.target.value)} />
+      </div>
+      <div>
+        <p className={styles.title}>本を探す</p>
+      </div>
+      {results.map((result, i) => (
+        <div className={`${results.length - 1 === i && styles.content__result__last}`} key={result.id}>
+          <Link href={`books/${result.id}`}>
+            <a>
+              <BookCard key={i} book={result} />
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BookSearchbar;
---
+++ components/sidebar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useAuth } from './AuthContext'
+import { useRouter } from 'next/router'
+import styles from '../styles/components/sidebar.module.css'
+
+
+const MenuList = ({ href, iconPath, text, currentPath }) => {
+  return (
+    <div className={styles.nav__menu__item}>
+      {/* Nav menu */}
+      <Link href={href}>
+        <a className={`${styles.nav__menu__link} ${href.includes(currentPath) && styles.nav__menu__link__active}`}>
+          <div className={styles.nav__menu__image__container}>
+            <Image src={iconPath} width="30" height="30" alt="icon" className={`${href.split('/')[1] === 'users' && styles.nav__menu__photo}`} />
+          </div>
+          <span className={styles.nav__menu__text}>{text}</span>
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+const SideBar = () => {
+  const { currentUser, logout } = useAuth()
+
+  const router = useRouter()
+
+  const { asPath } = router
+  const currentPath = asPath.split('/')[1]
+
+  const handleLogoutButton = () => {
+    logout()
+    router.push('/')
+  }
+
+  const handleOutput = () => {
+    router.push('/books/new1')
+  }
+
+  const links = [
+    { href: '/books/', iconPath: 'https://static.overlay-tech.com/assets/c5fee165-810f-4657-a7f5-0c8b69b98c1a.svg', text: 'ホーム' },
+    { href: '/message', iconPath: 'https://static.overlay-tech.com/assets/d704290d-0dc2-4c92-88c9-f094852e5678.svg', text: 'メッセージ' },
+    { href: `/users/${currentUser.uid}`, iconPath: `${currentUser.photoURL}`, text: 'プロフィール' },
+  ]
+
+  return (
+    <div className={styles.sidebar}>
+      {/* Nav Area */}
+      <nav className={styles.nav__menu}>
+        {/* Nav icons */}
+        {links.map(link => (
+          <MenuList key={link.href} href={link.href} iconPath={link.iconPath} text={link.text} currentPath={currentPath} />
+        ))}
+      </nav>
+
+      {/* Button Area */}
+      <div className={styles.btn__container}>
+        <button className={styles.btn__output} onClick={handleOutput}>
+          アウトプットする
+        </button>
+      </div>
+
+      <div className={styles.btn__container}>
+        <button className={styles.btn__logout} onClick={handleLogoutButton}>ログアウト</button>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar;
