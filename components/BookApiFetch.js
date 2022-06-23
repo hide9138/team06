7478e7ace964503bae449d7316e307bd35bd7e8b
efficient-life <efficient122@gmail.com>
2022-06-23 09:38:37 +0900

    BookAPI用関数作成

---
+++ components/BookApiFetch.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const GetBook = bookId => {
+	const [book, setBook] = useState([])
+	useEffect(() => {
+		const fetchData = async () => {
+			const url = 'https://www.googleapis.com/books/v1/volumes/' + bookId
+			const result = await axios(url)
+			const item = result.data
+			const outData = {
+				id: item.id,
+				title: item.volumeInfo.title,
+				authors: item.volumeInfo.authors,
+				description: item.volumeInfo.description,
+				pageCount: item.volumeInfo.pageCount,
+				smallImageLink: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.smallThumbnail : '',
+				ImageLink: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : '',
+			}
+			setBook(outData)
+		}
+		fetchData()
+	}, [])
+
+	return book
+}
+
+export const SerchBooks = serchWord => {
+	const [books, setBooks] = useState([])
+	useEffect(() => {
+		const fetchData = async () => {
+			const url = `https://www.googleapis.com/books/v1/volumes/?q=${serchWord}&maxResults=2&key=${process.env.NEXT_PUBLIC_BOOK_API_KEY}`
+			const result = await axios(url)
+			const items = result.data.items
+			const outData = items.map(item => {
+				return {
+					id: item.id,
+					title: item.volumeInfo.title,
+					authors: item.volumeInfo.authors,
+					description: item.volumeInfo.description,
+					pageCount: item.volumeInfo.pageCount,
+					smallImageLink: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.smallThumbnail : '',
+					ImageLink: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : '',
+				}
+			})
+			setBooks(outData)
+		}
+		fetchData()
+	}, [])
+
+	return books
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['static.overlay-tech.com','lh3.googleusercontent.com']
-  }
+	reactStrictMode: true,
+	images: {
+		domains: ['static.overlay-tech.com', 'lh3.googleusercontent.com', 'books.google.com'],
+	},
 }
+module.exports = nextConfig
