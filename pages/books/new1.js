8c5dca0e8bf3debd879326bdb4e68fc9ddd210b4
efficient-life <efficient122@gmail.com>
2022-06-24 09:13:43 +0900

    new1 本の取得new2遷移作成

--- pages/books/new1.js
+++ pages/books/new1.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-// import styles from '../../styles/Home.module.css'
 import Image from 'next/image'
 import styles from '../../styles/books/New1.module.css'
+import firebase, { db } from '../../firebase/firebase'
+import { useAuth } from '../../components/AuthContext'
 
 export default function Home() {
 	return (
@@ -19,25 +20,38 @@ export default function Home() {
 }
 
 // 本棚用（仮）
-const books = [
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
-	'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png',
-	'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png',
-	'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png',
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
-	'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png',
+const bookList = [
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/aad3ed2f-167e-4cf8-b56d-fbdd2d1294a8.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
+	{ id: 'LCIbnwEACAAJ', imageLink: 'https://static.overlay-tech.com/assets/bd07130e-33fa-4fcc-8f5f-be741bd81efa.png' },
 ]
 
 const BookShelf = () => {
+	const [books, setBooks] = useState(bookList)
+	const { currentUser } = useAuth()
+
+	useEffect(() => {
+		const userRef = db.collection('users').doc(currentUser.uid)
+		const userBooks = db.collection('books').where('userRef', '==', userRef).get()
+		console.log(userBooks)
+		// setBooks(userBooks)
+		return
+	}, [])
+
 	return (
 		<div className={styles.BookShelf}>
 			<div className={styles.bookShelfContainer}>
 				{books.map((book, i) => (
-					<div key={Math.random()} className={`${styles.bookShelfImage} ${i % 3 === 0 && styles.firstRowImage}`}>
-						<Image className={styles.bookImage} src={book} width="135" height="182" alt="book photo" />
+					<div key={book.id} className={`${styles.bookShelfImage} ${i % 3 === 0 && styles.firstRowImage}`}>
+						<Link href={{ pathname: `/books/new2`, query: { book_id: book.id } }}>
+							<Image className={styles.bookImage} src={book.imageLink} width="135" height="182" alt="book photo" />
+						</Link>
 					</div>
 				))}
 			</div>
