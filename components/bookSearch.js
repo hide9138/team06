54b97cc2d7a698fbeca3524b10e5ca722fe3ec2e
komei0727 <krymei072730@gmail.com>
2022-06-29 19:12:33 +0900

    implement book search

--- components/bookSearch.js
+++ components/bookSearch.js
@@ -5,32 +5,7 @@ import BookCard from './bookCard'
 import styles from '../styles/components/bookSearch.module.css'
 import firebase, { db } from '../firebase/firebase'
 import axios from 'axios'
-// import { SerchBooks } from './BookApiFetch'
 
-// type Book = {
-//   id: string
-//   title: string
-//   authors: string
-//   description: string
-//   pageCount: number
-//   smallImageLink: string
-//   ImageLink: string
-// }
-const SerchBooks = serchWord => {
-	const [books, setBooks] = useState([])
-	useEffect(() => {
-		const fetchData = async () => {
-			const url = `https://www.googleapis.com/books/v1/volumes/?q=${serchWord}&maxResults=2&key=${process.env.NEXT_PUBLIC_BOOK_API_KEY}`
-			const result = await axios(url)
-			const items = result.data.items
-			const outData = items.map(item => format(item))
-			setBooks(outData)
-		}
-		fetchData()
-	}, [serchWord])
-
-	return books
-}
 
 const BookSearchbar = memo(() => {
   // 入力値のデータ
@@ -39,14 +14,31 @@ const BookSearchbar = memo(() => {
   const [results, setResults] = useState([])
 
   const handleSearch = async (searchWord) => {
-    const searchedResults = SerchBooks(searchWord);
-    setResults(searchedResults)
+    const format = item => {
+      const info = item.volumeInfo
+      return {
+        id: item.id,
+        title: info.title,
+        authors: info.authors,
+        description: info.description,
+        pageCount: info.pageCount,
+        publisher: info.publisher,
+        publishedDate: info.publishedDate,
+        smallImageLink: info.imageLinks ? info.imageLinks.smallThumbnail : '',
+        imageLink: info.imageLinks ? info.imageLinks.thumbnail : '',
+      }
+    }
+    const url = `https://www.googleapis.com/books/v1/volumes/?q=${searchWord}&maxResults=20&key=${process.env.NEXT_PUBLIC_BOOK_API_KEY}`
+    const result = await axios(url)
+    const items = result.data.items
+    const outData = items.map(item => format(item))
+    setResults(outData)
   }
 
   useEffect(() => {
     const getResults = async () => {
-      const bookRefs = await db.collection('books').get()
-      const bookList = bookRefs.docs.map(querySnapshot => {
+      const tweetRefs = await db.collection('tweets').get()
+      const tweetList = tweetRefs.docs.map(querySnapshot => {
         return { ...querySnapshot.data(), id: querySnapshot.id }
       })
       const likeRefs = await db.collection('likes').get()
@@ -65,8 +57,8 @@ const BookSearchbar = memo(() => {
 			}
 
 			const results = Array.from(counts).map(([key, _]) => {
-				const book = bookList.filter(book => book.mainId == key)[0]
-				return book
+				const tweet = tweetList.filter(tweet => tweet.mainId == key)[0]
+				return tweet
 			})
 
 			// 取得したい分だけ取る
@@ -82,8 +74,7 @@ const BookSearchbar = memo(() => {
         <div className={styles.search__icon}>
           <Image src="/search.svg" width="16" height="16" alt="search icon" />
         </div>
-        <input className={styles.search__input} type="text" placeholder="本のタイトル、著者名" onChange={(e) => setSearchWord(e.target.value)} />
-        <button value={searchWord} onClick={(e) => handleSearch(e.target.value)}>検索</button>
+        <input className={styles.search__input} type="text" placeholder="本のタイトル、著者名" onKeyPress={(e) => handleSearch(e.target.value)} />
       </div>
       <div>
         <p className={styles.title}>本を探す</p>
